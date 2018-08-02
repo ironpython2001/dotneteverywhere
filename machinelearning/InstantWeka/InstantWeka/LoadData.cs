2ea6c883a52a3b0bf07342b6db9de9d7596374e0
ironpython <ironpython2001@gmail.com>
2018-08-02 17:56:58 +0530

    InstantWeka added

---
+++ machinelearning/InstantWeka/InstantWeka/LoadData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NTTU.BigODM.MachineLearning.Weka;
+using weka.core;
+using static weka.core.converters.ConverterUtils;
+
+namespace InstantWeka
+{
+    public class LoadData
+    {
+        public static void Run()
+        {
+            var source = new DataSource(@"dataset\titanic.arff");
+            var data = source.getDataSet();
+            Console.WriteLine(data.numInstances() + " instances loaded.");
+            Console.WriteLine(data.toString());
+        }
+    }
+}
---
+++ machinelearning/InstantWeka/InstantWeka/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace InstantWeka
+{
+    class Program
+    {
+        //https://github.com/limcheekin/instant-weka-howto
+        static void Main(string[] args)
+        {
+            LoadData.Run();
+            ReadLine();
+        }
+    }
+}
